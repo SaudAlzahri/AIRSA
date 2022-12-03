45153f141020370a9bc19a98cdcc990ed7f99ec7
SaudAlzahri <113842486+SaudAlzahri@users.noreply.github.com>
2022-12-03 03:05:01 +0300

    u

--- web.py
+++ web.py
@@ -27,13 +27,13 @@ original_titl = '<p style="font-family:Courier; color:Cyan; font-size: 30px;">TR
 st.sidebar.markdown(original_titl, unsafe_allow_html=True)
 
 oriinal_titl = '<p style="font-family:Courier; color:Yellow; font-size: 17px;">IMAGE URL</p>'
-img_url = st.sidebar.text_input(':frame_with_picture:' + oriinal_titl, unsafe_allow_html=True, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsP7xEOzp5Ii4nV4STAR9HjnVJqPfJLz1rSwqN-qsp&s')
+img_url = st.sidebar.text_input(oriinal_titl, unsafe_allow_html=True, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsP7xEOzp5Ii4nV4STAR9HjnVJqPfJLz1rSwqN-qsp&s')
 
 oriinl_titl = '<p style="font-family:Courier; color:Yellow; font-size: 17px;">ROAD WIDTH (METERS)</p>'
-roadwidth = int(st.sidebar.text_input(':car:' + oriinl_titl, unsafe_allow_html=True, '14'))
+roadwidth = int(st.sidebar.text_input(oriinl_titl, unsafe_allow_html=True, '14'))
 
 orinl_titl = '<p style="font-family:Courier; color:Yellow; font-size: 17px;">LANE COUNT</p>'
-lanecount = int(st.sidebar.text_input(':railway_track:' + orinl_titl, unsafe_allow_html=True, '5'))
+lanecount = int(st.sidebar.text_input(orinl_titl, unsafe_allow_html=True, '5'))
 
 
 

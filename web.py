ce9d1d4238766bc3b7e5727ff14056b1abd9b46d
SaudAlzahri <113842486+SaudAlzahri@users.noreply.github.com>
2022-12-03 03:11:30 +0300

    update

--- web.py
+++ web.py
@@ -24,17 +24,20 @@ st.write(
 
 st.sidebar.header(":gear:")
 original_titl = '<p style="font-family:Courier; color:Cyan; font-size: 30px;">TRY IT OUT!</p>'
-
 st.sidebar.markdown(str(original_titl), unsafe_allow_html=True)
 
+
 oriinal_titl = '<p style="font-family:Courier; color:Yellow; font-size: 17px;">IMAGE URL</p>'
-img_url = st.sidebar.text_input(str(oriinal_titl), unsafe_allow_html=True, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsP7xEOzp5Ii4nV4STAR9HjnVJqPfJLz1rSwqN-qsp&s')
+st.sidebar.markdown(str(oriinal_titl), unsafe_allow_html=True)
+img_url = st.sidebar.text_input('', unsafe_allow_html=True, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsP7xEOzp5Ii4nV4STAR9HjnVJqPfJLz1rSwqN-qsp&s')
 
 oriinl_titl = '<p style="font-family:Courier; color:Yellow; font-size: 17px;">ROAD WIDTH (METERS)</p>'
-roadwidth = int(st.sidebar.text_input(str(oriinl_titl), unsafe_allow_html=True, '14'))
+st.sidebar.markdown(str(oriinl_titl), unsafe_allow_html=True)
+roadwidth = int(st.sidebar.text_input('', unsafe_allow_html=True, '14'))
 
 orinl_titl = '<p style="font-family:Courier; color:Yellow; font-size: 17px;">LANE COUNT</p>'
-lanecount = int(st.sidebar.text_input(str(orinl_titl), unsafe_allow_html=True, '5'))
+st.sidebar.markdown(str(orinl_titl), unsafe_allow_html=True)
+lanecount = int(st.sidebar.text_input('', unsafe_allow_html=True, '5'))
 
 
 
